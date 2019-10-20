55803b3c7ad187a0cbbf9079fcf55506333a3afa
Robert Pittman <robertamoah271@gmail.com>
2019-10-20 08:54:54 -0400

    hello

--- server.js
+++ server.js
@@ -31,8 +31,11 @@ Handlebars.registerHelper("inc", function(value, options)
 
 
 // Routes
+import("./routes")
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
+
+
 var syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
@@ -44,9 +47,120 @@ if (process.env.NODE_ENV === "production") {
 }
 
 
+
+
+
+
+
+
+
+
+
+app.get("/profile/:id", (req, res) => {
+  let hbsobj = {}
+  orm.selectAUsersItems(req.params.id, (myItemsData) => {
+    hbsobj.myitems = myItemsData
+    orm.selectTopTen((topItemsData) => {
+      hbsobj.topitems = topItemsData  
+      console.log(hbsobj)
+      res.render("profile", hbsobj)
+    })
+  })
+})
+
+app.get("/", (req, res) => res.render("login",{}))
+
+  
+
+// Render 404 page for any unmatched routes
+app.get("*", function(req, res) {
+  res.send("404");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// nodemailer
+
+app.post("/form", (req, res) => {
+
+var email = req.body.email;
+var message = req.body.message
+var name = req.body.name;
+
+console.log("email: " + email, "name: " + name)
+
+const output =`<p> You have a new contact request </p>
+    <P> 😎Thank you for joing the squad 😎</p>
+    <p>From: ${req.body.fromUser}</p>
+    <p>Interest: ${req.body.interest}</p>
+    <p>${req.body.message}</p>
+    </ul>`;
+
+const nodemailer = require('nodemailer');
+
+let transporter =  nodemailer.createTransport({
+    service : "gmail",
+
+    auth: {
+        user: "bucketlistusa271@gmail.com",
+        pass: process.env.EMAIL_PW
+    }
+});
+
+
+
+let mailoptions  = {
+    from: "bucketlistusa271@gmail.com", // sender address
+    to: `${email}`, // list of receivers
+    subject: 'registration ✔', // Subject line
+    text: `Hey ${name}`, // plain text body
+    html: output
+};
+
+transporter.sendMail(mailoptions, (err, data) =>{
+if (err){
+    console.log(err)
+}else{ console.log("email sent")}
+});
+
+
+  res.send("its okay")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.get('/', function(req, res, next){
   res.sendStatus(200);
 });
+
+
+
+
+
 
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function(f) {
