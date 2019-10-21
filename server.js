d43748a2f89d0f383b6e78350fb6c11df74d9aaf
Robert Pittman <robertamoah271@gmail.com>
2019-10-21 09:11:13 -0400

    hello world

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ var syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "development") {
   syncOptions.force = true;
 
   app.get("/", (req, res) => res.render("login",{}))
